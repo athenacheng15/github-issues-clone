849556155a6341905dabe70629eb17c32c22924f
Athena Cheng <athenacheng@Hane-MacBook-Pro.local>
2022-10-15 12:53:30 +0800

    add Tag and StatusTag to storybook

---
+++ src/stories/Tag.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Tag from "../commons/Tag";
+
+export default {
+	title: "Tag",
+	component: Tag,
+	argTypes: {
+		text: {
+			options: ["Owner", "Author", "Collaborator"],
+			control: { type: "radio" },
+		},
+	},
+} as ComponentMeta<typeof Tag>;
+
+const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />;
+export const Default = Template.bind({});
+
+Default.args = {
+	text: "Owner",
+	display: true,
+};
